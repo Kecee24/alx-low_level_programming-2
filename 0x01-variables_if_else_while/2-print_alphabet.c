14be73b1f83e6a3abbffe47aecb4c3d9261ab41d
saviour623 <saviourme33@gmail.com>
2022-07-04 11:43:30 +0000

    Variable

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-char l;
-
-l = 'a';
- while (l <= 'z')
-   {
-      putchar(l);
-      l++;
-    }
-printf("\n");
-  
-return (0);
+        char l;
+	
+        l = 'a';
+	
+        while (l <= 'z')
+        {
+	  putchar(l);
+	  l++;
+	}
+        printf("\n");
+	return (0);
 }
