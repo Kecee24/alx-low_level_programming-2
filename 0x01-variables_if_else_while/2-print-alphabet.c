e4a4d43837a6198438de33931a5f87330c72ef46
saviour623 <saviourme33@gmail.com>
2022-07-04 07:20:36 +0000

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**main - Entry point
+ *
+ * return: Always zero (SUCCESS)
+ */
+int main(void)
+{
+  char alp;
+  
+  for (alp = 'a'; alp <= 'z'; alp++)
+    putchar(alp);
+  printf("\n");
+
+  return (0);
+}
