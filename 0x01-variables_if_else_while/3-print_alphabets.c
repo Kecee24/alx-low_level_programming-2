6d87123ceaa31d1b538b9910cbbb64fe1fdf1fa4
saviour623 <saviourme33@gmail.com>
2022-07-04 13:49:37 +0000

    uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char high;
+	char low;
+
+	high = 'A';
+	low = 'a';
+
+	while (low <= 'z')
+	{
+		putchar(low);
+		low++;
+	}
+
+	while (high <= 'Z')
+	{
+		putchar(high);
+		high++;
+	}
+	printf("\n");
+
+	return (0);
+}
